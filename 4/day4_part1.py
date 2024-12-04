225098a174e1b837d82182e950bc18a6ef4a83df
Jonathan Hook <jonathan.hook@york.ac.uk>
2024-12-04 08:55:37 +0000

    Part 1 complete

---
+++ 4/day4_part1.py
@@ -0,0 +1,57 @@
+from aocd import get_data
+
+def read_file(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+def parse_input(data: str) -> list[list[str]]:
+    return [list(line) for line in data.splitlines()]
+
+def search(grid: list[list[str]], x: int, y: int, dx: int, dy: int, found: str) -> int:
+    sx: int = x + dx
+    sy: int = y + dy
+
+    if sx < 0 or sy < 0 or sx >= len(grid[0]) or sy >= len(grid):
+        return 0
+
+    if len(found) > len('XMAS'):
+        return 0
+
+    found += grid[sy][sx]
+
+    if found == 'XMAS':
+        return 1
+    elif found[-1] != 'XMAS'[len(found)-1]:
+        return 0
+
+    result: int = 0
+    result += search(grid, sx, sy, dx, dy, found)
+
+    return result
+
+def task(data: str) -> int:
+    grid = parse_input(data)
+
+    result: int = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == 'X':
+                result += search(grid, x, y, 1, 0, "X")
+                result += search(grid, x, y, -1, 0, "X")
+                result += search(grid, x, y, 0, 1, "X")
+                result += search(grid, x, y, 0, -1, "X")
+                result += search(grid, x, y, 1, 1, "X")
+                result += search(grid, x, y, -1, -1, "X")
+                result += search(grid, x, y, 1, -1, "X")
+                result += search(grid, x, y, -1, 1, "X")
+
+    return result
+
+def test(data: str, expected: int) -> bool:
+    result: int = task(data)
+    return result == expected
+
+def main() -> None:
+    print(test(read_file('test.txt'), 18))
+    print(task(get_data(day=4, year=2024)))
+main()
